d1205d51cc54c5ab7343ebdaac01d9ac6204a5b5
Joel Blanco <jblnco.ing@example.com>
2021-01-30 12:23:53 -0400

    Se trabajarón store y update en recordController

--- app/Http/Controllers/Api/Record/RecordController.php
+++ app/Http/Controllers/Api/Record/RecordController.php
@@ -20,16 +20,6 @@ class RecordController extends ApiController
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -79,7 +69,7 @@ class RecordController extends ApiController
         ]);
         $record->users()->attach($request->input('user_id'));
         $record=Record::with('users')->find($record->id);
-        return $this->showOneData($record);
+        return $this->showOne($record);
     }
 
     /**
@@ -90,40 +80,62 @@ class RecordController extends ApiController
      */
     public function show(Record $record)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Record  $record
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Record $record)
-    {
-        //
+        
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Record  $record
+     * @param  int $record
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Record $record)
+    public function update(Request $request, $record)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Record  $record
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Record $record)
-    {
-        //
+        $record=Record::find($record);
+        $request->validate([
+            'room_id' => 'required|integer',
+            'date_in' => 'required|date',
+            'date_out' => 'required|date',
+            'user_id' => 'required|array',
+            'user_id.*' => 'integer',
+            'status' => 'boolean',
+        ]);
+        
+        $date_in=Carbon::parse($request->input('date_in'));
+        $date_out=Carbon::parse($request->input('date_out'));
+        $room_id=$request->input('room_id');
+        
+        if (Carbon::now()->gte($date_in))
+        return $this->errorResponse('The check-in date must be greater than or equal to the current date',400);
+        if ($date_in->gt($date_out))
+        return $this->errorResponse('Check-out date must be greater than check-in date',400);
+        
+        $exist=Record::where('room_id','=',$room_id)
+        ->where('id','!=',$record->id)
+        ->where(function ($query) use($date_in,$date_out) {
+            $query
+            ->whereDate('date_in','<=',$date_in)
+            ->whereDate('date_out','>=',$date_out)
+            ->orWhereBetween('date_in',[
+                $date_in,
+                $date_out
+            ])
+            ->orWhereBetween('date_out',[
+                $date_in,
+                $date_out
+            ]);
+        })->get();
+        if ($exist->isNotEmpty())
+            return $this->errorResponse('this room is not available for those dates',400);
+        $record->update([
+            'room_id' => $request->input('room_id'),
+            'date_in' => $request->input('date_in'),
+            'date_out' => $request->input('date_out'),
+            'status' => $request->input('status',1)
+        ]);
+        $record->users()->sync($request->input('user_id'));
+        $record=Record::with('users')->find($record->id);
+        return $this->showOneData($record);
     }
 }
