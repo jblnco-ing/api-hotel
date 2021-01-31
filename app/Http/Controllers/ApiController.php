b42962d3e880286deb5e234d69b883ff8d5d316a
Joel Blanco <jblnco.ing@example.com>
2021-01-31 11:12:03 -0400

    - Se eliminó el server heroku de la documentación - Se cambió la variable de entorno DB_DATABASE

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,12 +2,8 @@
 /**
 * @OA\Info(title="API Auth", version="1.0")
 * @OA\Server(
-*      url="http://127.0.0.1:8000/",
+*      url="http://localhost:8000/",
 *      description="Local Server"
-* )
-* @OA\Server(
-*    url="http://127.0.0.1:8000/",
-*    description="Heroku Server"
 * )
 */
 
